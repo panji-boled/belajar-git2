45a66356f1fab87cae4ab45db8217bc8ae35536d
Panji Sidik <55571232+panji-boled@users.noreply.github.com>
2025-10-13 17:00:20 +0700

    Update index.js add external 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Menuliskan perintah javascript external");
 console.log("Menuliskan perintah javascript external2");
 console.log("Menuliskan perintah javascript external3");
+console.log("Menuliskan perintah javascript external4");
