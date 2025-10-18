f9990e25cb0ffe60e37f42c9c397337f40b4247d
Panji Sidik <panjiplaydesk@gmail.com>
2025-10-18 16:52:25 +0700

    update fungsi panggilJS di index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ console.log("Menuliskan perintah javascript external");
 console.log("Menuliskan perintah javascript external2");
 console.log("Menuliskan perintah javascript external3");
 console.log("Menuliskan perintah javascript external4");
+
+function panggilJavascript() {
+    console.log("Menuliskan java script di external");
+  }
+
+  panggilJavascript();
+
+  function panggilJS(){
+    console.log("function kedua");
+  }
+
+  panggilJS();
